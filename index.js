6f8a47365a4b0a5fde256b89dd48492ac029a133
xhad <chyad@hotmail.com>
2016-12-07 21:30:43 +0900

    post error

--- index.js
+++ index.js
@@ -1,58 +1,51 @@
 // Chad Lynch - Seoul, South Korea
 
 const _Request = require('./lib/_request');
+const _request = new _Request(token, secret);
 
 class CoinOne {
   constructor(token, secret) {
-    this._request = new _Request(token, secret);
     this.v = '/v2';
   }
 
   orderbook(callback) {
-    this._request.get('/orderbook', callback);
+    _request.get('/orderbook', callback);
   }
 
   trades(callback) {
-    this._request.get('/trades', callback);
+    _request.get('/trades', callback);
   }
 
   ticker(callback) {
-    this._request.get('/ticker', callback);
+    _request.get('/ticker', callback);
   }
 
   userInfo(params, callback) {
-    return this._request
-      .post(this.v + '/account/' + 'user_info', params, callback);
+    _request.post(this.v + '/account/' + 'user_info', params, callback);
   }
 
   balance(params, callback) {
-    this._request
-      .post(this.v + '/account/' + 'balance', params, callback);
+    _request.post(this.v + '/account/' + 'balance', params, callback);
   }
 
   cancel(params, callback) {
-    this._request
-      .post(this.v + '/order/' + 'cancel', params, callback);
+    _request.post(this.v + '/order/' + 'cancel', params, callback);
   }
 
   limitBuy(params, callback) {
-    this._request
-      .post(this.v + '/order/' + 'limit_buy', params, callback);
+    _request.post(this.v + '/order/' + 'limit_buy', params, callback);
   }
 
   limitSell(params, callback) {
-    this._request
-      .post(this.v + '/order/' + 'limit_sell', params, callback);
+    _request.post(this.v + '/order/' + 'limit_sell', params, callback);
   }
 
   unfilledOrders(params, callback) {
-    this._request
-      .post(this.v + '/order/' + 'limit_orders', params, callback);
+    _request.post(this.v + '/order/' + 'limit_orders', params, callback);
   }
 
   filledOrders(params, callback) {
-    this._request
-      .post(this.v + '/order/' + 'complete_orders', params, callback);
+    _request.post(this.v + '/order/' + 'complete_orders', params, callback);
   }
 }
 
