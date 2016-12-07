b601cc281fa07158a884c5fe8c7874527a226904
xhad <chyad@hotmail.com>
2016-12-08 00:53:43 +0900

    de-sugared

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 // Chad Lynch - Seoul, South Korea
 
 const _Request = require('./lib/_request');
-const _request = new _Request(token, secret);
 
-class CoinOne {
-  constructor(token, secret) {
-    this.v = '/v2';
-  }
-
-  orderbook(callback) {
-    _request.get('/orderbook', callback);
-  }
-
-  trades(callback) {
-    _request.get('/trades', callback);
-  }
-
-  ticker(callback) {
-    _request.get('/ticker', callback);
-  }
-
-  userInfo(params, callback) {
-    _request.post(this.v + '/account/' + 'user_info', params, callback);
-  }
-
-  balance(params, callback) {
-    _request.post(this.v + '/account/' + 'balance', params, callback);
-  }
-
-  cancel(params, callback) {
-    _request.post(this.v + '/order/' + 'cancel', params, callback);
-  }
-
-  limitBuy(params, callback) {
-    _request.post(this.v + '/order/' + 'limit_buy', params, callback);
-  }
-
-  limitSell(params, callback) {
-    _request.post(this.v + '/order/' + 'limit_sell', params, callback);
-  }
-
-  unfilledOrders(params, callback) {
-    _request.post(this.v + '/order/' + 'limit_orders', params, callback);
-  }
-
-  filledOrders(params, callback) {
-    _request.post(this.v + '/order/' + 'complete_orders', params, callback);
-  }
+const CoinOne = function(token, secret) {
+  this._request = new _Request(token, secret);
+  this.v = '/v2';
 }
+
+CoinOne.prototype.orderbook = function(callback) {
+  this._request.get('/orderbook', callback);
+}
+
+CoinOne.prototype.trades = function(callback) {
+  this._request.get('/trades', callback);
+}
+
+CoinOne.prototype.ticker = function(callback) {
+  this._request.get('/ticker', callback);
+}
+
+CoinOne.prototype.userInfo = function(params, callback) {
+  this._request.post(this.v + '/account/' + 'user_info', params, callback);
+}
+
+CoinOne.prototype.balance = function(params, callback) {
+  this._request.post(this.v + '/account/' + 'balance', params, callback);
+}
+
+CoinOne.prototype.cancel = function(params, callback) {
+  this._request.post(this.v + '/order/' + 'cancel', params, callback);
+}
+
+CoinOne.prototype.limitBuy = function(params, callback) {
+  this._request.post(this.v + '/order/' + 'limit_buy', params, callback);
+}
+
+CoinOne.prototype.limitSell = function(params, callback) {
+  this._request.post(this.v + '/order/' + 'limit_sell', params, callback);
+}
+
+CoinOne.prototype.unfilledOrders = function(params, callback) {
+  this._request.post(this.v + '/order/' + 'limit_orders', params, callback);
+}
+
+CoinOne.prototype.filledOrders = function(params, callback) {
+  this._request.post(this.v + '/order/' + 'complete_orders', params, callback);
+}
+
+
 
 module.exports = CoinOne;
